d2a05727024ae6fdd8cba28c1b27a65feb8228e7
igor-pea <epgorokhovsky01@bvsd.org>
2017-07-11 12:56:23 -0600

    Added Main and Test files.
    Currently empty. Test file should eventually be replaced by unittests in each module.

---
+++ Time-Error-Correction/Main.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 11 12:54:21 2017
+
+@author: eliag
+"""
+
---
+++ Time-Error-Correction/Test.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+"""Testfile for testing the code."""
+#TODO: unittests should replace test file eventually.
+
