1efea81e07345b23d0d6ec7977c77431f569b393
Elia Gorokhovsky <epgorokhovsky01@bvsd.org>
2017-09-02 22:39:17 -0600

    Added indicator of experiment completion

--- Time-Error-Correction/Main.py
+++ Time-Error-Correction/Main.py
@@ -14,23 +14,26 @@ import Process
 import Systems
 
 #Preconditions for experiment
-assimilationMethods = ["RHF"]
+assimilationMethods = ["EAKF"]
 testingTypes = [True, False]
 stateErrors = [[0.01, 0.01, 0.01], [0.05, 0.05, 0.05], [0.1, 0.1, 0.1]]
-timeErrors = [0.001*i for i in range(0, 100)]
+timeErrors = [[0.001*i] for i in range(0, 10)]
 errors = [i+j for i in stateErrors for j in timeErrors]
 startTime = 0
-endTime = 200
-trials = 3
+endTime = 2
 observationIntervals = [0.5]
 systems = ["L63+time", "L84+time"]
+trials = 1
 
+totalTrials = len(assimilationMethods)*len(testingTypes)*len(stateErrors)*len(timeErrors)*len(observationIntervals)*len(systems)*trials
 #Writing to files
-filename = "" + ".csv"
+filename = "DataCollectionTest" + ".csv"
 file = open(filename, "a")
-file.write("testingType, system, assimilationMethod, observationInterval, stateError, timeError, RMSE")
+file.write("testingType, system, assimilationMethod, observationInterval, stateError, timeError, RMSE, \n")
 file.close()
 
+trialNum = 0
+print("Starting")
 #Python was not made for this many for loops :|
 for testingType in testingTypes:
     for system in systems:
@@ -38,7 +41,8 @@ for testingType in testingTypes:
             for observationInterval in observationIntervals:
                 for error in errors:
                     for trial in range(trials):
-                        Experiment = Process.Process(system=system, assimilation=assimilationMethod)
+                        Experiment = Process.Process(testingType)
+                        Experiment.choose_methods(system=system, assimilation=assimilationMethod)
                         truthList, timeList = Experiment.get_truth(startTime, endTime, 0.01, [1,1,1,0])
                         obsList, obsTimeList = Experiment.get_observations(observationInterval, error, ["State", "State", "State", "Time"])
                         ensembleList, ensembleTimeList = Experiment.run_ensemble(startTime, endTime, 0.01, 80, error, [1,1,1,0], obsList[0], [True, True, True, False], [1.5, 1.5, 1.5, 0])
@@ -46,8 +50,12 @@ for testingType in testingTypes:
                         rmse = AnalysisOperations.get_RMSE(ensembleMeans, truthList, [True, True, True, False])
                         #TODO: Move file writing to its own module
                         file = open(filename, "a")
-                        file.write(str(testingType) + "," + str(system) + "," + str(assimilationMethod) + "," + str(observationInterval) + "," + str(error[0]) + "," + str(error[3]) + "," + str(rmse))
+                        file.write(str(testingType) + "," + str(system) + "," + str(assimilationMethod) + "," + str(observationInterval) + "," + str(error[0]) + "," + str(error[3]) + "," + str(rmse) + ",\n")
                         file.close()    #Free up system resources and make sure not everything is lost if the code errors
+    
+                        #Print progress                        
+                        trialNum += 1
+                        print(str(round(100*trialNum/totalTrials, 2)) + "% done.")
                     
                 
             
